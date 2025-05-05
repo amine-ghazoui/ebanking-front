833eab8cde25f1cb46ce428d49b584b3df4a60a6
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-05-05 14:32:27 +0100

    Implemented CustomerService with HTTP methods (getCustomers and searchCustomers), integrated form handling in customers component, and improved UI with Bootstrap styling

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Customer } from '../model/custome.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-customers',
@@ -13,11 +14,17 @@ import { Customer } from '../model/custome.model';
 export class CustomersComponent implements OnInit{
 
   customers! : Observable<Array<Customer>>;
-  errorMessage! : string; // ou bien : Object | undefined
-  constructor(private customerService : CustomerService){}
+  errorMessage! : string; // ou bien : string | undefined
+  searchFormGroup : FormGroup | undefined;
+
+  constructor(private customerService : CustomerService, private fb : FormBuilder){}
 
 
   ngOnInit(): void {
+    
+    this.searchFormGroup = this.fb.group({
+      keyword : this.fb.control(""), 
+    });
     this.getCustomers();
   }
   
@@ -28,6 +35,10 @@ export class CustomersComponent implements OnInit{
         return throwError(err);
       })
     )
+  }
+
+  handleSearchCustomers(){
+
   }
 
 
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,20 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customer } from '../model/custome.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
+
   constructor(private http : HttpClient) {}
 
   getCustomers():Observable<Array<Customer>>{
-    return this.http.get<Array<Customer>>("http://localhost:8085/customers");
+    return this.http.get<Array<Customer>>(environment.backendHost+"/customers");
   }
 
-  serachCustomers():Observable<Array<Customer>>{
-    return this.http.get<Array<Customer>>("http://localhost:8085/customers/search");
+  serachCustomers(keyword : string):Observable<Array<Customer>>{
+    return this.http.get<Array<Customer>>(environment.backendHost+"/customers/search?keyword="+keyword);
   }
 }
 
